bad1d0c3e33f96e30f4b7c8512111f8fec547d3d
Irving2355 <it235597@gmail.com>
2024-04-29 11:47:31 -0600

    commit2

--- validar.php
+++ validar.php
@@ -9,6 +9,23 @@ $conexion=mysqli_connect('localhost','root','','world');
 
 //consulta
 
-$consulta= "SELECT * FROM USUARIOS WHERE name='$usuario' and contra='$pass'";
+$consulta= "SELECT * FROM USUARIOS WHERE NAME='$usuario' and CONTRA='$pass'";
+
+//resultado
+
+$resultado= mysqli_query($conexion, $consulta);
+
+$filas= mysqli_num_rows($resultado);
+
+if($filas >0){
+    header("location:../conexion/pagina2.html");
+}else{
+    echo '<script>
+        alert("¡¡Datos erroneos!!");
+    window.history.go(-1);
+    </script>';
+}
+
+mysqli_free_result($resultado);
 
 ?>
